cb02b57eff0b91e0ab02fc232b91a94c5b426171
sellnat77 <sell_nat@yahoo.com>
2017-02-15 23:31:54 -0800

    Initial iteration underway

--- app.py
+++ app.py
@@ -11,8 +11,16 @@ def index():
 
 @app.route('/stream')
 def stream():
+    musicFolder = os.curdir + '/static/music'
 
-    return render_template('stream.html')
+    mFiles = [f for f in os.listdir(musicFolder) if f.endswith('mp3')]
+    totalMFiles = len(mFiles)
+
+    return render_template('stream.html', MusicFiles=mFiles, TotalMFiles=totalMFiles)
+
+@app.route('/<filename>')
+def song(filename):
+    return render_template('play.html',MusicFile=filename)
 
 @app.route('/about')
 def about():
